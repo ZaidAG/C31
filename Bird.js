60864ef22648d73373a99884c14721938e1a523f
Zaid <gamerbrosimza@gmail.com>
2020-11-21 10:50:12 -0800

    smokebad for you

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeisbad=loadImage("sprites/smoke.png");
+    this.trajectory=[];
   }
 
   display() {
@@ -9,5 +11,12 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.velocity.x>10&&this.body.position.x>200){
+      var postionofeggs=[this.body.position.x,this.body.position.y];
+      this.trajectory.push(postionofeggs);
+    }
+    for(var i=0;i<this.trajectory.length;i++){
+      image(this.smokeisbad,this.trajectory[i][0],this.trajectory[i][1]);
+    }
   }
 }
---
+++ newSketch.js
@@ -0,0 +1,33 @@
+var temp1 = 5;
+console.log(temp1)
+
+var temp2 = "Hi Zaid";
+console.log(temp2)
+
+var temp3 = true;
+console.log(temp3);
+
+var temp4 = null;
+console.log(temp4);
+
+var temp5;
+console.log(temp5);
+
+var tempLala = [500,100,60,80];
+console.log(tempLala)
+
+console.log(tempLala.length);
+
+console.log(tempLala[0]);
+tempLala.push(700);
+tempLala.pop();
+
+console.log(tempLala[tempLala.length -1]);
+
+var temp7 = [["Zaid", 12],["Zoya",5]];
+
+console.log(temp7[1][0]);
+
+
+
+//
